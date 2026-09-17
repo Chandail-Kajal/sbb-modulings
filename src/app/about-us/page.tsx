8728dcce28b855a76b4aadcc9a600ff0603d0931
chandail-kajal <chandailkajal9@gmail.com>
2026-09-17 15:41:31 +0530

    blog and about us, cards swipe scroller

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client";
 
-import React, { useRef } from "react";
+import React, { useCallback } from "react";
 import Image from "next/image";
+import useEmblaCarousel from "embla-carousel-react";
+import AutoScroll from "embla-carousel-auto-scroll";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { Section } from "@/components/Section";
@@ -44,17 +46,31 @@ const cardsData: CardItem[] = [
 ];
 
 export default function Blog() {
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
+  // Triple the data so loop auto-scrolling is seamless and continuous
+  const displayCards = [...cardsData, ...cardsData, ...cardsData];
 
-  const handleScroll = (direction: "left" | "right") => {
-    if (scrollContainerRef.current) {
-      const scrollAmount = 380;
-      scrollContainerRef.current.scrollBy({
-        left: direction === "left" ? -scrollAmount : scrollAmount,
-        behavior: "smooth",
-      });
-    }
-  };
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    {
+      loop: true,
+      align: "start",
+      dragFree: true,
+    },
+    [
+      AutoScroll({
+        speed: 1,
+        stopOnInteraction: true,  // Pauses auto-scrolling when user swipes/drags
+        stopOnMouseEnter: true,   // Pauses on hover
+      }),
+    ]
+  );
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
   return (
     <div>
@@ -107,11 +123,11 @@ export default function Blog() {
           {/* Body Content */}
           <div className="space-y-6 font-ce text-neutral-600 text-fluid-16 leading-snug mb-16 w-full">
             <p className="w-full text-center">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.
             </p>
 
             <p className="w-full text-center">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.
             </p>
 
             <div className="space-y-2 pt-4 w-full">
@@ -119,7 +135,7 @@ export default function Blog() {
                 Precision Assembly. Built for Production.
               </h3>
               <p className="w-full text-center text-fluid-16 leading-snug">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.
               </p>
             </div>
 
@@ -128,7 +144,7 @@ export default function Blog() {
                 Precision Assembly. Built for Production.
               </h3>
               <p className="w-full text-center">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.
               </p>
             </div>
           </div>
@@ -139,7 +155,7 @@ export default function Blog() {
             <div className="order-2 sm:order-1 flex justify-center sm:justify-end items-center gap-3 mt-6 sm:mt-0 sm:mb-6">
               <button
                 type="button"
-                onClick={() => handleScroll("left")}
+                onClick={scrollPrev}
                 aria-label="Previous slide"
                 className="group w-10 h-10 rounded-full bg-neutral-200 hover:bg-black active:scale-95 flex items-center justify-center transition-colors duration-200"
               >
@@ -148,7 +164,7 @@ export default function Blog() {
 
               <button
                 type="button"
-                onClick={() => handleScroll("right")}
+                onClick={scrollNext}
                 aria-label="Next slide"
                 className="group w-10 h-10 rounded-full bg-neutral-200 hover:bg-black active:scale-95 flex items-center justify-center transition-colors duration-200"
               >
@@ -156,37 +172,39 @@ export default function Blog() {
               </button>
             </div>
 
-            {/* Cards Carousel: order-1 on mobile (top), order-2 on desktop (bottom) */}
+            {/* Swipeable & Auto-scrolling Viewport */}
             <div
-              ref={scrollContainerRef}
-              className="order-1 sm:order-2 flex items-start gap-6 overflow-x-auto scroll-smooth pb-4 no-scrollbar"
-              style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+              ref={emblaRef}
+              className="order-1 sm:order-2 overflow-hidden select-none cursor-grab active:cursor-grabbing pb-4"
             >
-              {cardsData.map((card) => (
-                <div
-                  key={card.id}
-                  className="min-w-70 sm:min-w-85 md:min-w-95 flex-1 shrink-0 flex flex-col space-y-4"
-                >
-                  <div className="relative w-full aspect-16/10 rounded-[1.75rem] overflow-hidden bg-neutral-100 shadow-sm">
-                    <Image
-                      src={card.imageSrc}
-                      alt={card.imageAlt}
-                      fill
-                      className="object-cover object-center hover:scale-105 transition-transform duration-300"
-                      sizes="(max-width: 768px) 85vw, 33vw"
-                    />
-                  </div>
+              <div className="flex -ml-6 touch-pan-y">
+                {displayCards.map((card, index) => (
+                  <div
+                    key={`${card.id}-${index}`}
+                    className="flex-[0_0_85%] sm:flex-[0_0_48%] lg:flex-[0_0_32%] min-w-0 pl-6 flex flex-col space-y-4"
+                  >
+                    <div className="relative w-full aspect-16/10 rounded-[1.75rem] overflow-hidden bg-neutral-100 shadow-sm pointer-events-none">
+                      <Image
+                        src={card.imageSrc}
+                        alt={card.imageAlt}
+                        fill
+                        draggable={false}
+                        className="object-cover object-center"
+                        sizes="(max-width: 768px) 85vw, (max-width: 1024px) 50vw, 33vw"
+                      />
+                    </div>
 
-                  <div className="space-y-1.5 w-full">
-                    <h3 className="w-full text-left font-bold text-neutral-900 text-fluid-24 leading-snug">
-                      {card.title}
-                    </h3>
-                    <p className="w-full text-center font-ce text-fluid-16 text-neutral-500 leading-snug">
-                      {card.description}
-                    </p>
+                    <div className="space-y-1.5 w-full pointer-events-none">
+                      <h3 className="w-full text-left font-bold text-neutral-900 text-fluid-24 leading-snug">
+                        {card.title}
+                      </h3>
+                      <p className="w-full text-left font-ce text-fluid-16 text-neutral-500 leading-snug">
+                        {card.description}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
--- src/components/manufacturing-capabilities/production.tsx
+++ src/components/manufacturing-capabilities/production.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 "use client";
 import Image from "next/image";
 import { Section } from "../Section";
@@ -65,7 +66,7 @@ export default function Production() {
           </div>
         </div>
       </Section>
-      <div className=" mb-(--section-y)">
+      <div className="mb-(--section-y)">
         <CapabilitiesCarousel capabilities={capabilities} />
       </div>
     </div>
@@ -87,6 +88,7 @@ interface CapabilitiesCarouselProps {
 function CapabilitiesCarousel({ capabilities }: CapabilitiesCarouselProps) {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
+  // Triple items for continuous infinite scroll feel
   const extendedCapabilities = [
     ...capabilities,
     ...capabilities,
@@ -98,14 +100,16 @@ function CapabilitiesCarousel({ capabilities }: CapabilitiesCarouselProps) {
       loop: true,
       align: "center",
       skipSnaps: false,
+      dragFree: false,
+      containScroll: false,
     },
     [
       AutoScroll({
         speed: 1,
-        stopOnInteraction: false,
-        stopOnMouseEnter: true,
+        stopOnInteraction: true,     // stops when user swipes so gesture doesn't fight auto-scroll
+        stopOnMouseEnter: true,      // pauses on hover
       }),
-    ],
+    ]
   );
 
   const onSelect = useCallback(() => {
@@ -115,53 +119,67 @@ function CapabilitiesCarousel({ capabilities }: CapabilitiesCarouselProps) {
 
   useEffect(() => {
     if (!emblaApi) return;
-    // eslint-disable-next-line react-hooks/set-state-in-effect
     onSelect();
     emblaApi.on("select", onSelect);
     emblaApi.on("reInit", onSelect);
   }, [emblaApi, onSelect]);
 
+  const scrollTo = useCallback(
+    (index: number) => {
+      if (!emblaApi) return;
+      emblaApi.scrollTo(index);
+    },
+    [emblaApi]
+  );
+
   return (
-    <div className="w-full overflow-hidden mb-(--section-y)" ref={emblaRef}>
-      <div className="flex -ml-4 touch-pan-y py-4">
+    <div
+      className="w-full overflow-hidden select-none cursor-grab active:cursor-grabbing"
+      ref={emblaRef}
+    >
+      <div className="flex -ml-4 touch-pan-y py-6">
         {extendedCapabilities.map((item, index) => {
           const isSelected = index === selectedIndex;
 
           return (
             <div
-              key={item.id || index}
+              key={`${item.id}-${index}`}
+              onClick={() => scrollTo(index)}
               className="flex-[0_0_85%] sm:flex-[0_0_60%] md:flex-[0_0_50%] lg:flex-[0_0_40%] pl-4 min-w-0"
             >
-              <div className="group flex flex-col items-center text-center cursor-pointer w-full">
+              <div className="group flex flex-col items-center text-center w-full">
+                {/* Capsule Card (Swipe target & clickable) */}
                 <div className="w-full flex items-center justify-center mb-6">
                   <div
-                    className={`w-full xl:h-65 sm:h-55 h-50 relative rounded-full border-4 transition-all duration-500 ${
-                      isSelected ? "border-[#0052cc]" : "border-transparent"
+                    className={`w-full xl:h-64 sm:h-56 h-48 relative rounded-full p-1 border-4 transition-all duration-500 ease-out ${
+                      isSelected
+                        ? "border-[#0052cc] scale-100 shadow-lg shadow-blue-500/10"
+                        : "border-transparent scale-95 opacity-60 hover:opacity-85"
                     }`}
                   >
                     <div
                       className={`relative w-full h-full rounded-full overflow-hidden transition-all duration-500 ${
-                        isSelected
-                          ? "blur-none scale-100 opacity-100"
-                          : "blur-[3px] scale-95 opacity-60"
+                        isSelected ? "blur-none" : "blur-[2px]"
                       }`}
                     >
                       <Image
                         src={item.imageSrc}
                         alt={item.imageAlt}
                         fill
-                        className="object-cover object-center transition-transform duration-500 group-hover:scale-105"
-                        sizes="(max-width: 768px) 100vw, 33vw"
+                        draggable={false} // Prevents native browser image dragging from blocking touch swipe
+                        className="object-cover object-center transition-transform duration-500 group-hover:scale-105 pointer-events-none"
+                        sizes="(max-width: 768px) 85vw, 40vw"
                       />
                     </div>
                   </div>
                 </div>
 
+                {/* Text Section */}
                 <h3
                   className={`text-fluid-32 font-bold tracking-tight leading-none mb-2 transition-all duration-500 ${
                     isSelected
-                      ? "text-neutral-800 blur-none opacity-100"
-                      : "text-neutral-400 blur-[1px] opacity-60"
+                      ? "text-neutral-900 opacity-100"
+                      : "text-neutral-400 opacity-60"
                   }`}
                 >
                   {item.title}
@@ -169,8 +187,8 @@ function CapabilitiesCarousel({ capabilities }: CapabilitiesCarouselProps) {
                 <p
                   className={`text-fluid-16 max-w-70 leading-snug transition-all duration-500 ${
                     isSelected
-                      ? "text-neutral-500 blur-none opacity-100"
-                      : "text-neutral-400 blur-[1px] opacity-60"
+                      ? "text-neutral-600 opacity-100"
+                      : "text-neutral-400 opacity-60"
                   }`}
                 >
                   {item.description}
