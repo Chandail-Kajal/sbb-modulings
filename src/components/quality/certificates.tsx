0680d49aefb1732b224a1a6694f6267323e2cafd
Chandail-Kajal <153283465+Chandail-Kajal@users.noreply.github.com>
2026-09-26 14:21:29 +0900

    height in certificate image

--- src/components/quality/certificates.tsx
+++ src/components/quality/certificates.tsx
@@ -34,7 +34,7 @@ export function Certificate() {
     <Section className="w-full pb-(--section-y)" disablePaddingY>
       <div className="section-container">
         <div className="relative flex flex-col pt-8 sm:pt-12 lg:flex-row lg:items-end">
-          <div className="relative z-10 h w-full shrink-0 overflow-hidden rounded-[26px] bg-neutral-900 shadow-lg sm:h-120 lg:h-145 lg:w-[40%] xl:w-[38%]">
+          <div className="relative z-10 h w-full shrink-0 overflow-hidden rounded-[26px] bg-neutral-900 shadow-lg sm:h-120 lg:h-145 xl:h-160 lg:w-[40%] xl:w-[38%]">
             <Image
               src="/quality/handshake.png"
               alt="Business handshake"
